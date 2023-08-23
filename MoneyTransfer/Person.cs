ca87ba8ce91b2bf12703d167c41ee5789d185182
midnightrips <sg3584@alum.barnard.edu>
2023-08-23 10:26:10 -0400

    Initialization of code on GitHub

---
+++ MoneyTransfer/Person.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransfer
+{
+    internal class Person
+    {
+        public string name;
+        public Wallet wallet; //changed it from private to public...
+        public int amountToTransfer;
+        public Person giver;
+        public Person receiver;
+
+
+        public Person(string name, int cash)
+        {
+            //TODO 2: Assign the value of the 'name' parameter to the 'name' member variable. One line.
+
+            this.name = name;
+
+            //TODO 3: Instantiate a new 'Wallet' object, passing the 'cash' parameter into its constructor. Assign this new Wallet to the 'wallet' member variable. One line.
+            wallet = new Wallet(cash); //wallet never saves the money in the wallet
+
+        }
+
+        public void DisplayInfo()
+        {
+            //TODO 4: Use string interpolation in a ConsoleWriteLine to display the user's name and also the amount of money in their wallet (using dot notation). One line.
+            Console.WriteLine($"{name} has ${wallet.money}");
+        }
+
+        public void AcceptMoney(int amount)
+        {
+            //TODO 7: Use an assignment operator to increase the amount of money in the person's wallet by the value of the 'amount' parameter. One line.
+            wallet.money = wallet.money + amount;
+        }
+
+        public bool TransferMoney(Person receiver, int amountToTransfer)
+        {
+            /*TODO 8: Use a conditional to check if the 'amountToTransfer' parameter of this method is less than or equal 
+             * to the amount of money in the wallet of the giver. 
+             * If the parameter is less than the amount in the wallet, subtract that amount from the wallet, then 
+             * call the receiver's AcceptMoney() method and pass 'amountToTransfer' into it. 
+             * Also use a Console.WriteLine to describe the transaction that occurred.
+             * If the parameter is greater than the amount in the giver's wallet, do not transfer any money and 
+             * instead print a message describing why the transfer failed.
+             */
+
+
+            if (amountToTransfer <= wallet.money) // <= giver money -> how do I specify this is relative to brotherOne's wallet?
+            {
+                wallet.money = wallet.money - amountToTransfer; //giver's wallet is subtracted from
+                receiver.AcceptMoney(amountToTransfer);
+                Console.WriteLine($"{name} is sending {receiver.name} ${amountToTransfer}.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Transfer Declined: {name} has insufficient funds.");
+                return false;
+            }
+
+        }
+
+        public Person ChooseGiver()
+        {
+            Simulation giveMoney = new Simulation();
+            string giverChoiceInitial = Console.ReadLine();
+            string giverChoice = giverChoiceInitial.ToLower();
+
+            if (giverChoice == "1" || giverChoice == "albert")
+            {
+                giver = giveMoney.brotherOne;
+            }
+            else if (giverChoice == "2" || giverChoice == "jacob")
+            {
+                giver = giveMoney.brotherTwo;
+            }
+            else if (giverChoice == "3" || giverChoice == "amelia")
+            {
+                giver = giveMoney.sister;
+            }
+            else if (giverChoice == "4" || giverChoice == "larisa")
+            {
+                giver = giveMoney.mother;
+            }
+            else if (giverChoice == "5" || giverChoice == "gennady")
+            {
+                giver = giveMoney.father;
+            }
+            //else if (giverChoice == "6" || giverChoice == "you" || giverChoice == "me")
+            //{
+            //    giver = giveMoney.you;
+            //}
+            else
+            {
+                Console.WriteLine($"{giverChoiceInitial} is not an option. Please try again.");
+            }
+            
+            return giver;
+
+        }
+
+        public Person ChooseReceiver()
+        {
+            Simulation getMoney = new Simulation();
+            string receiverChoiceInitial = Console.ReadLine();
+            string receiverChoice = receiverChoiceInitial.ToLower();
+
+            if (receiverChoice == "1" || receiverChoice == "albert") //code for if they choose themselves
+            {
+                receiver = getMoney.brotherOne;
+            }
+            else if (receiverChoice == "2" || receiverChoice == "jacob")
+            {
+                receiver = getMoney.brotherTwo;
+            }
+            else if (receiverChoice == "3" || receiverChoice == "amelia")
+            {
+                receiver = getMoney.sister;
+            }
+            else if (receiverChoice == "4" || receiverChoice == "larisa")
+            {
+                receiver = getMoney.mother;
+            }
+            else if (receiverChoice == "5" || receiverChoice == "gennady")
+            {
+                receiver = getMoney.father;
+            }
+            //else if (receiverChoice == "6" || receiverChoice == "you" || receiverChoice == "me")
+            //{
+            //    receiver = getMoney.you;
+            //}
+            else
+            {
+                Console.WriteLine($"{receiverChoiceInitial} is not an option. Please try again.");
+            }
+
+            return receiver;
+
+        }
+
+        public int ChooseAmount()
+        {
+            int amountToTransfer = 0;
+            bool goodChoice = false;
+
+            while (goodChoice == false)
+            {
+                string transferChoice = Console.ReadLine();
+                amountToTransfer = int.Parse(transferChoice);
+                goodChoice = true;
+                //add a conditional that accounts for inputs that aren't just whole numbers
+            }
+
+            return amountToTransfer;
+        }
+    }
+}
---
+++ MoneyTransfer/Simulation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransfer
+{
+    internal class Simulation
+    {
+
+        public Person brotherOne = new Person("Albert", 5000);
+        public Person brotherTwo = new Person("Jacob", 1000);
+        public Person sister = new Person("Amelia", 300);
+        public Person mother = new Person("Larisa", 200000);
+        public Person father = new Person("Gennady", 500000);
+        public Person you = new Person("you", 500); //I added an extra...going to make this a userInput situation where they can add their own name
+        public Simulation()
+        {
+            //Person brotherOne = new Person("Albert", 5000);
+            //Person brotherTwo = new Person("Jacob", 1000);
+            //Person sister = new Person("Amelia", 300);
+            //Person mother = new Person("Larisa", 200000);
+            //Person father = new Person("Gennady", 500000);
+            //Person you = new Person("you", 500);
+        }
+
+        /* Bonus story:
+         * Add 3 additional Person objects to the RunSimulation() method and expand the simulation to provide the user a menu.
+         * The menu should allow the user to select any of the 5 people and have them transfer a balance the user selects to one of the other people.
+         */
+        public void RunSimulation()
+        {
+            Console.WriteLine("Starting simulation");
+
+            Console.WriteLine("Choose a family member that will transfer away some of their money:");
+            Console.WriteLine($"1: {brotherOne.name}");
+            Console.WriteLine($"2: {brotherTwo.name}");
+            Console.WriteLine($"3: {sister.name}");
+            Console.WriteLine($"4: {mother.name}");
+            Console.WriteLine($"5: {father.name}");
+            //Console.WriteLine($"6: {you.name}");
+            Person giver = brotherOne.ChooseGiver();
+            giver.DisplayInfo();
+
+            Console.WriteLine($"Now choose a family member that will receive money from {giver.name}:");
+            Console.WriteLine($"1: {brotherOne.name}");
+            Console.WriteLine($"2: {brotherTwo.name}");
+            Console.WriteLine($"3: {sister.name}");
+            Console.WriteLine($"4: {mother.name}");
+            Console.WriteLine($"5: {father.name}");
+            //Console.WriteLine($"6: {you.name}");
+            Person receiver = brotherOne.ChooseReceiver();
+            receiver.DisplayInfo();
+
+            Console.WriteLine($"How many USD would you like {giver.name} to transfer to {receiver.name}?");
+            Console.WriteLine("(Hint: input whole numbers only and without symbols or punctuation (e.g. 10000))");
+            int amountToTransfer = brotherOne.ChooseAmount();
+
+
+            giver.TransferMoney(receiver, amountToTransfer);
+
+            //TODO 10: Call the .DisplayInfo() method on each person object again, showing that each person's balance has changed. Two lines.
+            giver.DisplayInfo();
+            receiver.DisplayInfo();
+
+            Console.WriteLine("Simulation complete.");
+        }
+    }
+}
